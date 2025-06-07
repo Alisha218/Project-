2f236bd28dd36a46fb873a98617a9c45aa419efa
Alisha218 <bscs2312105@szabist.pk>
2025-06-07 11:08:52 +0000

    create API

---
+++ Welfare_Org/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const pool  = require('./db');
+require('dotenv').config();
+
+const app= express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/donors', async (req, res) => {
+    try {
+        const [donors] = await pool.query('SELECT * FROM Donors');
+        res.json(donors);
+    } catch (err) {
+        res.status(500).json({Error: err.message});
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT;
+app.listen(PORT,()=>{
+    console.log(`Connected Sucesfully on PORT ${PORT}`); 
+})
